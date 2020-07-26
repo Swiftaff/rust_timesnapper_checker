529e2f6aeaf4f7f8401ecf26db04e896bfb6dac0
swiftaff <swiftaff@gmail.com>
2020-07-26 17:45:48 +0800

    notification shows hours and mins

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ impl SystemTray {
         }
     }
 
-    fn get_config_path_from_timesnapper_ini(&self) -> String {
+    fn get_property_from_timesnapper_ini(&self, property: &str) -> String {
         //first get path from this apps config using confy
         let config_path = &self.get_config_path_from_confy();
 
@@ -63,7 +63,7 @@ impl SystemTray {
         match result_conf {
             Ok(conf) => {
                 let section: &ini::ini::Properties = conf.section(None::<String>).unwrap();
-               section.get("Path").unwrap().to_string()
+               section.get(property).unwrap().to_string()
             }
             Err(e) => match e {
                 Error::Io(io) => "This is probably an error because we do not know where your Timesnapper Settings.ini is\r\nYou should find it here:\r\nC:\\Users\\<your-user>\\AppData\\Roaming\\TimeSnapper\\Settings.ini\r\nPlease update your config.".to_string(),
@@ -73,57 +73,109 @@ impl SystemTray {
     }
 
     fn about(&self) {
-        let config_path = &self.get_config_path_from_timesnapper_ini();
+        let config_path = &self.get_property_from_timesnapper_ini("Path");
         let content = format!("This tool will read the contents of a folder at the specified path.\r\nYou can update the path via the config file.\r\nIt's probably saved here\r\n'C:/Users/<yourusername>/AppData/Roaming/rust-win-gui/config'\r\n\r\nCurrent Path is:\r\n{:?}", config_path);
         nwg::simple_message("Settings", &content);
     }
 
     fn todays_stats(&self) {
         //nwg::simple_message("Hello", &self.get_todays_stats());
-        nwg::simple_message("Hello", &self.get_todays_snapshots_folder());
+        //nwg::simple_message("Hello", &self.get_todays_snapshots_folder());
+        self.get_todays_snapshots_folder()
     }
 
     fn notification(&self) {
         let flags = nwg::TrayNotificationFlags::USER_ICON | nwg::TrayNotificationFlags::LARGE_ICON;
-        self.tray
-            .show("Hello World", Some("testy"), Some(flags), Some(&self.icon2));
+        self.tray.show(
+            "Timesnapper checker",
+            Some("testy"),
+            Some(flags),
+            Some(&self.icon2),
+        );
     }
 
-    fn get_todays_snapshots_folder(&self) -> String {
-        self.get_all_folders()
-    }
-
-    fn get_all_folders(&self) -> String {
-        let config_path = &self.get_config_path_from_timesnapper_ini();
-        let result = &self.get_vec_direntries();
+    fn get_todays_snapshots_folder(&self) {
+        let config_path = self.get_property_from_timesnapper_ini("Path");
+        let interval_string = self.get_property_from_timesnapper_ini("Interval");
+        let result = &self.get_vec_direntries(config_path.clone());
         match result {
             Ok(vec_direntries) => {
-                let todays_directory = &self.get_todays_directory(vec_direntries);
-                println!("ok {:?}", todays_directory);
+                let result_todays_directory_path = &self.get_todays_directory_path(vec_direntries);
+                let files = &self.get_todays_directory_files(result_todays_directory_path);
+
+                //TODO
+                //get x screensperminute from timesnapper ini
+                //count total, total x screensperminute for notification
+                let count = files.len() as u32;
+                let result_interval: Result<u32, std::num::ParseIntError> = interval_string.parse();
+                let interval = match result_interval {
+                    Ok(i) => i,
+                    Err(e) => 999,
+                };
+                let total = count * interval;
+                let total_hours = (total / 60);
+                let remaining_minutes = total - (total_hours * 60);
+                let str_time = if total_hours == 0 {
+                    format!("{:?}mins", total)
+                } else if remaining_minutes == 0 {
+                    if total_hours == 1 {
+                        format!("{:?}hr", total_hours)
+                    } else {
+                        format!("{:?}hrs", total_hours)
+                    }
+                } else {
+                    if total_hours == 1 {
+                        format!("{:?}hr {:?}mins", total_hours, remaining_minutes)
+                    } else {
+                        format!("{:?}hrs {:?}mins", total_hours, remaining_minutes)
+                    }
+                };
+                //sort files by date added, filter by last hour, count number below X in size in last hour - to notify about black ones
+
+                //elsewhere - run this every x minutes
+
+                //allow editing: ini location
+
+                let flags =
+                    nwg::TrayNotificationFlags::USER_ICON | nwg::TrayNotificationFlags::LARGE_ICON;
+                self.tray.show(
+                    "Timesnapper checker",
+                    Some(&format!("{} so far today", str_time)),
+                    Some(flags),
+                    Some(&self.icon2),
+                );
+
+                println!("count:{}", count);
+                println!("interval:{:?}", interval);
+                println!("total:{:?}", total);
+                println!("str_time:{:?}", str_time);
+                println!("files: {:?}", files);
             }
-            Err(e) => println!("error from -- {} -- {:?}", config_path, e),
+            Err(e) => println!(
+                "error from get_vec_direntries -- {} -- {:?}",
+                config_path, e
+            ),
         }
-        format!("get all folders from {}", &config_path)
+        //format!("get all folders from {}", &config_path)
     }
 
-    fn get_vec_direntries(&self) -> Result<Vec<std::fs::DirEntry>, io::Error> {
-        let config_path = &self.get_config_path_from_timesnapper_ini();
-        let result = fs::read_dir(config_path)?
+    fn get_vec_direntries(&self, config_path: String) -> Result<Vec<std::fs::DirEntry>, io::Error> {
+        let result = fs::read_dir(&config_path)?
             //.filter_map(|r| r.ok())
             //.map(|res| res.map(|e| e.metadata()))
             .collect::<Result<Vec<std::fs::DirEntry>, io::Error>>()?;
         Ok(result)
     }
 
-    fn get_vec_metadata(&self) -> Result<Result<Vec<std::fs::Metadata>, io::Error>, io::Error> {
-        let config_path = &self.get_config_path_from_timesnapper_ini();
+    /*fn get_vec_metadata(&self) -> Result<Result<Vec<std::fs::Metadata>, io::Error>, io::Error> {
+        let config_path = &self.get_property_from_timesnapper_ini();
         let result = fs::read_dir(config_path)?
             .map(|res| res.map(|e| e.metadata()))
             .collect::<Result<Result<Vec<std::fs::Metadata>, io::Error>, io::Error>>()?;
         Ok(result)
-    }
+    }*/
 
-    fn get_todays_directory<'a>(
+    fn get_todays_directory_path<'a>(
         &self,
         vec_direntries: &'a Vec<std::fs::DirEntry>,
     ) -> Result<&'a std::fs::DirEntry, String> {
@@ -151,22 +203,32 @@ impl SystemTray {
         } else {
             Err(format!("{:?}", x))
         }
-        //println!("{:?}", x);
-        /*
-        for n in 0..vec_direntries.len() {
-            let mut fname = vec_direntries[n].file_name().into_string();
-            match fname {
-                Ok(d) => {
-                    if d == todays_date_as_string {
-                        break;
-                        Some((todays_date_as_string, vec_direntries[n]))
+    }
+
+    fn get_todays_directory_files<'a>(
+        &self,
+        result_todays_directory: &Result<&'a std::fs::DirEntry, String>,
+    ) -> Vec<std::fs::DirEntry> {
+        match result_todays_directory {
+            Ok(direntry) => {
+                let path = direntry.path(); //format!("{}", direntry.path());
+
+                let result =
+                    self.get_vec_direntries(path.as_os_str().to_str().unwrap().to_string());
+                println!("path {:?}", path);
+                match result {
+                    Ok(vec_dir_entries) => vec_dir_entries,
+                    Err(e) => {
+                        println!("Error {:?}", e);
+                        Vec::new()
                     }
                 }
-                error => None,
+            }
+            Err(e) => {
+                println!("Error {:?}", e);
+                Vec::new()
             }
         }
-        None*/
-        //todays_date_as_string
     }
 
     /*fn get_todays_stats(&self) -> String {
